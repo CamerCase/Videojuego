83b2aef3920eae9058ef790fc16fa65fe229fa61
CamerCase <mastergd627@gmail.com>
2026-05-25 15:19:07 -0500

    NewCode

---
+++ Game.fs
@@ -0,0 +1,40 @@
+module App.Game
+open System 
+open System.Threading
+open App.Types
+open App.Utils
+
+let updateTick =
+    createUpdateTick (fun s -> s.Tick) (fun t s -> {s with Tick = t})
+
+let drawAlien state =
+    match state.AlienState with
+    | Alive -> displayMessage state.AlienX state.AlienY ConsoleColor.Green "🤖"
+    | Hit -> displayMessage state.AlienX state.AlienY ConsoleColor.Red "💥"
+    state
+let drawMisiles state =
+    state.Misiles
+    |> List.iter (fun misil -> displayMessage misil.X misil.Y ConsoleColor.Cyan "=>")
+    state
+
+let drawEnemy state =
+    match state.EnemyState with
+    | Alive -> displayMessage state.EnemyX state.EnemyY ConsoleColor.Yellow "👾"
+    | Hit -> displayMessage state.EnemyX state.EnemyY ConsoleColor.Red "💥"
+    state
+
+let drawMisilesEnemigos state =
+    state.MisilesEnemigos
+    |> List.iter (fun misil -> displayMessage misil.X misil.Y ConsoleColor.Red "<=")
+    state
+
+
+
+
+let drawGame = [|
+    drawAlien
+    drawMisiles
+    drawEnemy
+    drawMisilesEnemigos
+|]
+let drawGameLoop = createRedrawScreen drawGame (fun s -> s.RedrawScreen) (fun s -> { s with RedrawScreen = false })
--- Program.fs
+++ Program.fs
@@ -1,1 +1,7 @@
-﻿
+﻿module App.Program
+open System 
+open App.Types
+open App.Utils
+open App.Menu
+
+mostrar() |> ignore
