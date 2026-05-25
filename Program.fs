058deaca2c57a2f1b8dd613682757c4928be096d
CamerCase <mastergd627@gmail.com>
2026-05-25 16:00:21 -0500

    PruebaJson

--- Program.fs
+++ Program.fs
@@ -4,4 +4,13 @@ open App.Types
 open App.Utils
 open App.Menu
 
+let estadoPrueba = { initialState with Score = 5; Lives = 2 }
+App.Save.guardar estadoPrueba
+printfn "Guardado OK"
+
+
+match App.Save.cargar initialState with
+| Some s -> printfn "Cargado: Score=%d Lives=%d" s.Score s.Lives
+| None   -> printfn "No hay guardado"
+
 mostrar() |> ignore
